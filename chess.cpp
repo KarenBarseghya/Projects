90a06b41eaa36d3a8f77527bb67fbbbedfb65515
Karen Barseghyan <87120803+KarenBarseghya@users.noreply.github.com>
2022-08-15 16:59:58 +0400

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -5,10 +5,15 @@ int main()
 {
     std::string figure;
     std::string next_place;
+    std::cout << "When you want restart game enter 0";    
     board_symbols();
     while(true)
     {
         std::cin >> figure;
+        if(figure == 0)
+        {
+            return 0;
+        }
         std::cin >> next_place;
         checker(figure , next_place);
         board_printer();
